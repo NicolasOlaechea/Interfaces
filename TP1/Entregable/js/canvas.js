500bec3226084d0dd8615eabf81c1fec4b7fecdf
NicolasOlaechea <nicoolaechea11@gmail.com>
2021-09-08 16:26:43 -0300

    Soluciono el problema del cambio de color al borrar o dibujar

--- TP1/Entregable/js/canvas.js
+++ TP1/Entregable/js/canvas.js
@@ -1,18 +1,23 @@
+"use strict";
+
+//Creo las variable globales
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let widthCanvas = canvas.width;
+let heightCanvas = canvas.height;
 let rectCanvas = canvas.getBoundingClientRect(); //Coordenadas iniciales del canvas
 let x = 0; 
 let y = 0;
-let widthCanvas = canvas.width;
-let heightCanvas = canvas.height;
 let dibujando = false;
 let borrando = false;
-let color = document.getElementById("color_lapiz");
+let color;
 let grosor = document.getElementById("grosor_lapiz");
 let lapiz = document.getElementById("logo_lapiz");
 let goma = document.getElementById("logo_goma");
 let archivo = document.getElementById("archivo");
+let btnLimpiarLienzo = document.getElementById("btnLimpiarLienzo");
 
+//Le asigno los eventos a los elementos correspondientes
 archivo.addEventListener("change", function(e){
     let reader = new FileReader();
     reader.readAsDataURL(e.target.files[0]);
@@ -22,15 +27,22 @@ archivo.addEventListener("change", function(e){
 })
 
 lapiz.addEventListener("click", function(){
+    borrando = false; //Para cambiar el valor de la variable color cuando dibuja
     permitirDibujar();
 })
 
 goma.addEventListener("click", function(){
-    permitirBorrar();
+    borrando = true; //Para cambiar el valor de la variable color cuando borra
+    permitirDibujar();
 })
 
+btnLimpiarLienzo.addEventListener("click", function(){
+    limpiarLienzo();
+})
+
+//Creo las funciones
 function permitirDibujar(){
-    goma.removeEventListener("click", permitirBorrar);
+    //Le asigno al canvas distintos eventos para poder dibujar sobre él
     canvas.addEventListener("mousedown", function(e){ //Cuando se hace click dentro del canvas
         x = e.clientX - rectCanvas.left;
         y = e.clientY - rectCanvas.top;
@@ -38,7 +50,7 @@ function permitirDibujar(){
     })
         
     canvas.addEventListener("mousemove", function(e){//Cuando se mueve el mouse dentro del canvas
-        if((dibujando===true) && (borrando===false)){
+        if(dibujando===true){
             dibujar(x, y, e.clientX - rectCanvas.left, e.clientY - rectCanvas.top);
             x = e.clientX - rectCanvas.left;
             y = e.clientY - rectCanvas.top;
@@ -56,46 +68,12 @@ function permitirDibujar(){
 }
 
 function dibujar(x1, y1, x2, y2){
-    console.log("dibujo "+color.value);
     ctx.beginPath();
-    ctx.strokeStyle = color.value;
-    ctx.lineWidth = grosor.value;
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-    ctx.closePath();
-}
-
-function permitirBorrar(){
-    lapiz.removeEventListener("click", permitirDibujar);
-    canvas.addEventListener("mousedown", function(e){
-        x = e.clientX - rectCanvas.left;
-        y = e.clientY - rectCanvas.top;
-        dibujando = true;
-    })
-        
-    canvas.addEventListener("mousemove", function(e){
-        if(dibujando===true){
-            borrar(x, y, e.clientX - rectCanvas.left, e.clientY - rectCanvas.top);
-            x = e.clientX - rectCanvas.left;
-            y = e.clientY - rectCanvas.top;
-        }
-    })
-        
-    canvas.addEventListener("mouseup", function(e){
-        if(dibujando===true){
-            borrar(x, y, e.clientX - rectCanvas.left, e.clientY - rectCanvas.top);
-            x = 0;
-            y = 0;
-            dibujando = false;
-        }
-    })
-}
-
-function borrar(x1, y1, x2, y2){
-    color = "#ffffff";
-    console.log("borro "+color);
-    ctx.beginPath();
+    if(borrando===true){
+        color = "#ffffff";
+    }else{
+        color = document.getElementById("color_lapiz").value;
+    }
     ctx.strokeStyle = color;
     ctx.lineWidth = grosor.value;
     ctx.moveTo(x1, y1);
@@ -105,13 +83,9 @@ function borrar(x1, y1, x2, y2){
 }
 
 function limpiarLienzo(){
+    //Limpio el canvas completamente
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
-
-let btnLimpiarLienzo = document.getElementById("btnLimpiarLienzo");
-btnLimpiarLienzo.addEventListener("click", function(){
-    limpiarLienzo();
-})
 
 function insertarImagen(reader){
     let imagen = new Image();
